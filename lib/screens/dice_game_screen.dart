7ef907e069a0a00c5456b9daaa10ad4b7f910e72
Faith Ye En Xin <faith.ye94@gmail.com>
2023-10-11 14:36:33 +0800

    feat(dice): add in dice roll functionality with setState

--- lib/screens/dice_game_screen.dart
+++ lib/screens/dice_game_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class DiceGameScreen extends StatefulWidget {
@@ -8,14 +10,71 @@ class DiceGameScreen extends StatefulWidget {
 }
 
 class _DiceGameScreenState extends State<DiceGameScreen> {
+  var currenDiceRoll = 1;
+  void rollDice() {
+    setState(() {
+      currenDiceRoll = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Counter'),
+          title: const Text('Dice Game'),
         ),
-        body: Center(
-          child: Image.asset("assets/images/dice-1.png", width: 200),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Center(
+              child: Image.asset("assets/images/dice-$currenDiceRoll.png",
+                  width: 200),
+            ),
+            TextButton(
+              onPressed: rollDice,
+              style: ButtonStyle(
+                textStyle: MaterialStateProperty.all<TextStyle>(const TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                )),
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.blue
+                          .withOpacity(0.8); // Custom color for pressed state
+                    } else if (states.contains(MaterialState.disabled)) {
+                      return Colors.grey; // Custom color for disabled state
+                    }
+                    return Colors.blue; // Default color for other states
+                  },
+                ),
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                  const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+                ),
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32.0),
+                    side: const BorderSide(color: Colors.blue),
+                  ),
+                ),
+                side: MaterialStateProperty.all<BorderSide>(
+                  const BorderSide(color: Colors.blue, width: 2.0),
+                ),
+                overlayColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.hovered)) {
+                      return Colors.blue.withOpacity(
+                          0.2); // Custom overlay color for hovered state
+                    }
+                    return Colors.black; // No overlay color for other states
+                  },
+                ),
+              ),
+              child: const Text('Roll Dice'),
+            )
+          ],
         ));
   }
 }
